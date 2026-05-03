8cfdd426d42557b0d31190c14022ee761b62d57d
Dylan Schoop <dylanschoop@gmail.com>
2026-05-03 14:45:49 +0200

    Added System Prompt, Markdown rendering, Tokens, Structured Output and Chat History per user

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,83 +1,167 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { micromark } from "micromark";
+import DOMPurify from "dompurify";
 
 function App() {
-  const [message, setMessage] = useState("");
-  const [messages, setMessages] = useState([
-    { text: "Hey, I'm Relmy! I assist in world-building. What do you need help with?", sender: "bot" }
-  ]);
-  const [loading, setLoading] = useState(false);
+    const [userId, setUserId] = useState("");
+    const [messages, setMessages] = useState([]);
+    const [message, setMessage] = useState("");
+    const [world, setWorld] = useState({})
+    const [loading, setLoading] = useState(false);
 
-  const changeMessage = (e) => {
-    setMessage(e.target.value);
-  };
+    useEffect(() => {
+        let storedUserId = localStorage.getItem("userid");
 
-  const sendMessage = async () => {
-    setLoading(true);
+        if (!storedUserId) {
+            storedUserId = crypto.randomUUID();
+            localStorage.setItem("userid", storedUserId);
+        }
 
-    if (!message.trim()) {
-        setLoading(false);
-        return;
+        setUserId(storedUserId);
+
+        fetch("http://localhost:3000/api/gethistory", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ userId: storedUserId })
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            if (data.length > 0) {
+                setMessages(data);
+            } else {
+                setMessages([
+                    {
+                        text: `## Hey, I'm Relmy!\nI help with world-building.`,
+                        sender: "bot"
+                    }
+                ]);
+            }
+        });
+        
+        setWorld({
+            locations: [],
+            history: []
+        });
+    }, []);
+
+    const changeMessage = (e) => {
+        setMessage(e.target.value);
     };
 
-    const newMessages = [
-      ...messages,
-      { text: message, sender: "user" },
-      { text: "Generating...", sender: "bot" }
-    ];
+    const sendMessage = async () => {
+        setLoading(true);
 
-    setMessages(newMessages);
-
-    try {
-      const response = await fetch("http://localhost:3000/api/chat", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ message })
-      });
-
-      const data = await response.json();
-
-      setMessages(prev => {
-        const updated = [...prev];
-        updated[updated.length - 1] = {
-          text: data.response,
-          sender: "bot"
+        if (!message.trim()) {
+            setLoading(false);
+            return;
         };
-        return updated;
-      });
-    } catch (err) {
-      console.error(err);
-    }
 
-    setMessage("");
+        const newMessages = [
+            ...messages,
+            { text: message, sender: "user" },
+            { text: "Generating...", sender: "bot" }
+        ];
 
-    setLoading(false);
-  };
+        setMessages(newMessages);
 
-  return (
-    <div className="max-w-xl mx-auto">
-      <h1 className="p-4 font-bold text-2xl">Taalmodel</h1>
+        try {
+            const response = await fetch("http://localhost:3000/api/chat", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ userId, message })
+            });
 
-      {/* Chat-box */}
-      <div className="p-4 space-y-2">
-        {messages.map((msg, index) => (
-          <p
-            key={index}
-            className={msg.sender === "user" ? "text-right" : "text-left"}
-          >
-            {msg.text}
-          </p>
-        ))}
-      </div>
+            const data = await response.json();
+            
+            console.log(data);
 
-      {/* Chat-message */}
-      <div className="flex p-4">
-        <input type="text" className="flex-1 p-2 border rounded" placeholder="Type something..." value={message} onChange={changeMessage}/>
-        <button className="px-4 py-2 bg-blue-500 text-white rounded disabled:opacity-50 ml-2" onClick={sendMessage} disabled={loading}>SEND</button>
-      </div>
-    </div>
-  );
+            setMessages(prev => {
+                const updated = [...prev];
+                updated[updated.length - 1] = {
+                    text: data.message,
+                    sender: "bot",
+                    tokens: data.tokens
+                };
+                return updated;
+            });
+
+            setWorld({
+                locations: data.locations,
+                history: data.history,
+            });
+        } catch (err) {
+            console.error(err);
+        }
+
+        setMessage("");
+
+        setLoading(false);
+    };
+
+    return (
+        <div className="max-w-xl mx-auto">
+            <h1 className="p-4 font-bold text-2xl">Relmy</h1>
+
+            {/* Chat-box */}
+            <div className="p-4 space-y-2">
+                {messages.map((msg, index) => (
+                    <div
+                        key={index}
+                        className={(
+                            msg.sender === "user"
+                            ? "text-right bg-blue-500"
+                            : "text-left bg-gray-800"
+                        ) + " p-3 rounded-lg"}
+                    >
+                      <div
+                          className="text-white"
+                          dangerouslySetInnerHTML={{
+                              __html: DOMPurify.sanitize(micromark(msg.text))
+                          }}
+                      />
+                      {msg.tokens && <p className={(msg.sender === "user" ? "text-left": "text-right") + " text-sm"}>Tokens: {msg.tokens}</p>}
+                    </div>
+                ))}
+            </div>
+
+            {/* Chat-message */}
+            <div className="flex p-4">
+                <input type="text" className="flex-1 p-2 border rounded" placeholder="Type something..." value={message} onChange={changeMessage}/>
+                <button className="px-4 py-2 bg-blue-500 text-white rounded disabled:opacity-50 ml-2" onClick={sendMessage} disabled={loading}>SEND</button>
+            </div>
+            
+            <h1 className="p-4 font-bold text-2xl">World</h1>
+
+            <div className="p-4 text-left">
+                {world.locations && (
+                    <div>
+                        <p>Locations:</p>
+                        <ul>
+                            {world.locations.map((loc, index) => (
+                                <li key={index}>{loc}</li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
+
+                {world.history && (
+                    <div>
+                        <p>History:</p>
+                        <ul>
+                            {world.history.map((event, index) => (
+                                <li key={index}>{event}</li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import { callAssistant } from './chat.js'
+import { getChatHistory, callAssistant } from './chat.js'
 
 const app = express()
 
@@ -8,17 +8,23 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static("public"));
 app.get("/", (req, res) => {
-  res.sendFile("public/index.html", { root: "." });
+    res.sendFile("public/index.html", { root: "." });
 });
 app.listen(3000, () => console.log(`Serving on http://localhost:3000`))
 
 app.get('/api/test', async (req, res) => {
-    const response = await callAssistant("What is the core instinct of humans?")
-    res.json({ response })
+    const response = await callAssistant("dln-01", "What is your function?")
+    res.json(response)
 })
 
+app.post("/api/gethistory", (req, res) => {
+    const { userId } = req.body;
+    const response = getChatHistory(userId);
+    res.json(response);
+});
+
 app.post('/api/chat', async(req, res) => {
-    const { message } = req.body
-    const response = await callAssistant(message)
-    res.json({response})
+    const { userId, message } = req.body
+    const response = await callAssistant(userId, message)
+    res.json(response)
 })
